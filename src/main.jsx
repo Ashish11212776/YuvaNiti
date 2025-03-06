b7c9eaa97b26139dd2f19df5a7218bd029447f31
Ashish11212776 <ashish.kumar@avisoft.io>
2025-03-06 12:32:38 +0530

    f2

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { BrowserRouter} from 'react-router-dom';
-import Navbar from "./Components/Pages/Navbar.jsx"
-import Footer from './Components/Pages/Footer.jsx';
+import Navbar from "./components/Pages/Navbar.jsx"
+import Footer from './components/Pages/Footer.jsx';
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './features/store.js'
